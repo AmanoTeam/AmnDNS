5b3c77047a9328cb4c30ca111cf9ca3fbf6d5c6d
SnwMds <snwmds@protonmail.com>
2020-08-28 02:28:31 -0300

    update update-blacklist.sh

--- scripts/update-blacklist.sh
+++ scripts/update-blacklist.sh
@@ -7,19 +7,28 @@ curl --url 'https://block.energized.pro/extensions/regional/formats/unbound.conf
 	--url 'https://block.energized.pro/extensions/xtreme/formats/unbound.conf' \
 	--url 'https://block.energized.pro/unified/formats/unbound.conf' \
 	--url 'https://raw.githubusercontent.com/AmanoTeam/AmnDNS/master/etc/unbound/unbound.conf.d/blacklist.conf' \
-	--request 'GET' \
-	--http2-prior-knowledge \
 	--silent \
 	--doh-url 'https://1.0.0.1/dns-query' \
 	--ipv4 \
 	--connect-timeout '25' \
 	--no-sessionid \
-	--no-keepalive | awk 'NF && !seen[$0]++' > 'blacklist.conf'
+	--no-keepalive \
+	| awk 'NF && !seen[$0]++' \
+	| sed -r '/(<|>|\t|#|\}|\)|;)/d; s/\\//g' \
+	| sed -r 's/\.{2,}/./g; s/" static/" always_nxdomain/g' \
+	> 'blacklist.conf'
 
-unbound-checkconf 'blacklist.conf'
+mv '/etc/unbound/unbound.conf.d/blacklist.conf' 'blacklist.conf.backup'
+mv 'blacklist.conf' '/etc/unbound/unbound.conf.d/blacklist.conf'
+
+unbound-checkconf '/etc/unbound/unbound.conf'
 
 if [ "${?}" = '0' ]; then
-	mv 'blacklist.conf' '/etc/unbound/unbound.conf.d/blacklist.conf'
 	systemctl restart unbound
+	echo "done"
+else
+	mv 'blacklist.conf.backup' '/etc/unbound/unbound.conf.d/blacklist.conf'
+	echo "error"
+fi
 
 exit '0'
